eaf0235c5248f6845fb3a33d9a33ae90b9c983fe
Akhil.Ramani <you@example.com>
2019-03-06 10:22:36 +0530

    on cancel bug fixed

--- lib/socketHandlers/file.sockethandler.ts
+++ lib/socketHandlers/file.sockethandler.ts
@@ -1,8 +1,5 @@
-import { statSync, open, write, createReadStream, createWriteStream, unlink, fstat } from 'fs';
+import { statSync, open, write, createReadStream, createWriteStream, unlink } from 'fs';
 import { generateThumb } from './file.thumbnail';
-
-// let tempPath = join(__dirname, '../public/files/temp');
-// let uploadPath = join(__dirname, '../public/files')
 
 export class SocketUpload{
     private socket:any;
@@ -28,7 +25,7 @@ export class SocketUpload{
                     place = stat.size / 524288;
                 }
             }
-            catch(err){ }
+            catch(err){ console.log('New file arrived') }
             open(`${this.uploadPath}/${fileName}`, 'a', 0o755, (err, fd) => {
                 if(err) return console.log(err);
                 this.files[fileName]['handler'] = fd;
@@ -44,7 +41,7 @@ export class SocketUpload{
             if(this.files[fileName]['downloaded'] == this.files[fileName]['fileSize']){ //file fully uploaded
                 write(this.files[fileName]['handler'], this.files[fileName]['data'], null, 'Binary', (err, written) => {
                     //Thumbnail
-                    generateThumb(`${this.uploadPath}/${fileName}`, `${this.thumbPath}/`)
+                    //generateThumb(`${this.uploadPath}/${fileName}`, `${this.thumbPath}/`)
                     // let inStream = createReadStream(`${tempPath}/${fileName}`);
                     // let outStream = createWriteStream(`${this.uploadPath}/${fileName}`);
                     // inStream.pipe(outStream);
@@ -75,24 +72,30 @@ export class SocketUpload{
         })
 
         this.socket.on('cancel', async (data: any)=> {
-            console.log(data);
             let count:number = 0 ;
             try{
-                await data.files.forEach((fileName: string) => {
-                    let stat = statSync(`${this.uploadPath}/${fileName}`);
-                    if(stat.isFile()){
-                        unlink(`${this.uploadPath}/${fileName}`, () => {
-                            console.log('in --',count);
-                            count = count + 1;
-                        })
+                let filesArr = data.files;
+                for (let i = 0; i< filesArr.length; i++){
+                    let stat = statSync(`${this.uploadPath}/${filesArr[i]}`);
+                    if (stat.isFile()){
+                        await new Promise((resolve, reject) => {
+                            unlink(`${this.uploadPath}/${filesArr[i]}`, () => {
+                                count = count + 1;
+                                console.log('Delete ', count)
+                                resolve()
+                            })
+                        }).catch(() => console.log('deletion err'))
                     }
-                });
-
-                console.log('cancel done', count);
+                    if(i === filesArr.length-1){
+                        console.log('All files cancel done');
+                        socket.emit('cancel-done', {count})
+                    }
+                }
             }
-            catch(e){console.log(e)}
-            
-            socket.emit('cancel-done', { count });
+            catch(e){
+                console.log('No such file');
+                socket.emit('cancel-done', {count});
+            }
         })
 
         this.socket.on('disconnect', ()=>{
